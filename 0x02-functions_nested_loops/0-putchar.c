1249ffbe8468437b9de48fa8c3c4e27aa07ebf3c
Waigwa254 <augustinewaigwa254@gmail.com>
2023-09-12 06:26:02 -0700

    character print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,12 @@
 
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+_putchar('_');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('a');
+_putchar('\n');
 
     return (0);
 }
