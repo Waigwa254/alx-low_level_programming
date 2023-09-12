262897582d72c2b3ab724395f8fd4da892964725
Waigwa254 <augustinewaigwa254@gmail.com>
2023-09-12 08:13:48 -0700

    islower ascii

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,11 +7,11 @@
 	 * Return: 1 if char is lowercase, otherwise 0.
 	 */
 
-
-	int _islower(int c)
+int _islower(int c)
+{
+	if (c >= 97 && c <=122)
 	{
-		if (c >= 'a' && c <= 'z')
-			return (1);
-		else
-			return (0);
+	return (1);
 	}
+        return (0);
+}
