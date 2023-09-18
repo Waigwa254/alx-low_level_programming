27f3dcddee4f92c03dd2f1e1b5daeba44f24e1a8
Waigwa254 <augustinewaigwa254@gmail.com>
2023-09-18 13:35:48 -0700

    task 3-9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,5 +11,8 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+#include <stdio.h>
+#include <unistd.h>
 
 #endif
