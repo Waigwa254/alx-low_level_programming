9d82ca4733165ec9de20d54753ebdf8e15d10202
Waigwa254 <augustinewaigwa254@gmail.com>
2023-09-18 11:03:56 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+int start = 0;
+while (s[start])
+start++;
+while (start--)
+_putchar(s[start]);
+_putchar('\n');
+}
