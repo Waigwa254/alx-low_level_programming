685c1f44ba9ba3b2cd5b3991636704d54d673247
Waigwa254 <augustinewaigwa254@gmail.com>
2023-10-19 02:46:28 -0700

    returns the number of elements in a linked list_t

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,10 @@ size_t nodes;
 nodes = 0;
 while (h != NULL)
 {
-if (h ->str == NULL)
+if (h->str == NULL)
 printf("[%d] %s\n", 0, "(nil)");
 else
-printf("[%d] %s\n", h ->len, h->str);
+printf("[%d] %s\n", h->len, h->str);
 h = h->next;
 nodes++;
 }
