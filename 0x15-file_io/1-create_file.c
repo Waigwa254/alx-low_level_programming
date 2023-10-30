f3308209161262e21ebe2d505417ff6d4abe7e43
Waigwa254 <augustinewaigwa254@gmail.com>
2023-10-30 10:43:37 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ if (text_content != NULL)
 for (len = 0; text_content[len];)
 len++;
 }
-fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 06000);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 w = write(fd, text_content, len);
 if (fd == -1 || w == -1)
 return (-1);
