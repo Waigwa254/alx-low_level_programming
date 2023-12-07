eb6c33e459653f6a88605af39ea369370c127ea9
Waigwa254 <augustinewaigwa254@gmail.com>
2023-12-07 01:45:02 -0800

    task 2-8

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns linnkedd llist
+ * @head: pointer
+ * @index: index
+ * Return: nthb
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int size;
+	dlistint_t *tmp;
+	size = 0;
+	if (head == NULL)
+		return (NULL);
+	tmp = head;
+	while (tmp)
+	{
+		if (index == size)
+			return (tmp);
+		size++;
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
