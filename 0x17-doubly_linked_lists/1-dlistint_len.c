57bdbca0df8ca36beb66918a1606efff1653d0b7
Waigwa254 <augustinewaigwa254@gmail.com>
2023-12-07 08:48:54 -0800

    dlistint

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,15 +7,20 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
+
 	count = 0;
+
 	if (h == NULL)
 		return (count);
-	while (h-> prev != NULL)
+
+	while (h->prev != NULL)
 		h = h->prev;
+
 	while (h != NULL)
 	{
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
