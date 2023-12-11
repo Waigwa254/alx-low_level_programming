9e192874b7c1109dc0d097f8d4180dbf5f920b94
Waigwa254 <augustinewaigwa254@gmail.com>
2023-12-11 01:10:23 -0800

    dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/calculation.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/**
+ * sum - sum of 2 int
+ * @x: 1 unit
+ * @y: 2 unit
+ * Return: sum 
+ */
+int sum(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * division - divv of 2 int
+ * @x: 1 unit
+ * @y: 2 unit
+ * Return: division
+ */
+int division(int x, int y)
+{
+        return (x / y);
+}
+
+/**
+ * minus - min of 2 int
+ * @x: 1 unit
+ * @y: 2 unit
+ * Return: min
+ */
+int minus(int x, int y)
+{
+        return (x - y);
+}
+
+/**
+ * times - mult of 2 int
+ * @x: 1 unit
+ * @y: 2 unit
+ * Return:times
+ */
+int times(int x, int y)
+{
+        return (x * y);
+}
+
+/**
+ * modd - modd of 2 int
+ * @x: 1 unit
+ * @y: 2 unit
+ * Return: mod
+ */
+int modd(int x, int y)
+{
+        return (x % y);
+}
+
+
