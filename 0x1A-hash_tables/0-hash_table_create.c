985c980368b84a25b7b585ad42b8fbf5693581f1
Waigwa254 <augustinewaigwa254@gmail.com>
2023-12-21 08:10:53 -0800

    4 cmt

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,22 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - hash
- * @size: sixe
- * Return: NULL
+ * hash_table_create - table
+ * @size: able
+ *
+ * Return:ble
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *ht;
-unsigned long int i;
-ht = malloc(sizeof(hash_table_t));
-if (ht == NULL)
-return (NULL);
-ht->size = size;
-ht->array = malloc(sizeof(hash_node_t *) * size);
-if (ht->array == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-ht->array[i] = NULL;
-return (ht);
+	hash_table_t *table;
+
+	if (size == 0)
+		return (NULL);
+
+	table = calloc(1, sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
 }
